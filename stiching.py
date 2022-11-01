25b096fd03d9f698fd330c0e62a063a407ec3e4a
windy82581 <windy82581@gmail.com>
2022-11-01 23:26:41 +0900

    make

---
+++ stiching.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+import os
+import glob
+import sys
+
+img_names = glob.glob('./4camera/*.png')
+imgs = []
+# 불러온 영상을 imgs에 저장
+for name in img_names:
+    img = cv2.imread(name)
+    if img is None:
+        print('Image load failed!')
+        sys.exit()
+        
+    imgs.append(img)
+
+
+# 객체 생성
+stitcher = cv2.Stitcher_create(cv2.Stitcher_PANORAMA)
+
+# 이미지 스티칭
+status, dst = stitcher.stitch(imgs)
+
+if status != cv2.Stitcher_OK:
+    print('Stitch failed!')
+    sys.exit()
+    
+# 결과 영상 저장
+cv2.imwrite('output.jpg', dst)
+
+# 출력 영상이 화면보다 커질 가능성이 있어 WINDOW_NORMAL 지정
+cv2.namedWindow('dst', cv2.WINDOW_NORMAL)
+cv2.imshow('dst',dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
---
+++ 탑뷰.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import glob
+from undistort import *
+from cv2 import FONT_HERSHEY_COMPLEX
+
+
+
+def topview(img_original):    
+        
+    chessboardx=5
+    chessboardy=5
+    CHECKERBOARD = (chessboardx,chessboardy)
+    img = img_original
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, cv2.CALIB_CB_ADAPTIVE_THRESH+cv2.CALIB_CB_FAST_CHECK+cv2.CALIB_CB_NORMALIZE_IMAGE)
+    print(ret)
+    corner1x = corners[0][0][0]
+    corner1y = corners[0][0][1]
+    corner2x = corners[chessboardx-1][0][0]
+    corner2y = corners[chessboardx-1][0][1]
+    corner3x = corners[(chessboardx)*(chessboardy-1)][0][0]
+    corner3y = corners[(chessboardx)*(chessboardy-1)][0][1]
+    corner4x = corners[(chessboardx)*(chessboardy)-1][0][0]
+    corner4y = corners[(chessboardx)*(chessboardy)-1][0][1]
+
+    pts = np.array([[corner1x, corner1y], [corner2x, corner2y], [corner3x, corner3y], [corner4x, corner4y]], dtype=np.float32)
+    idx=0
+    for pt in pts:
+        idx+=1
+        cv2.circle(img, tuple(pt.astype(np.int)), 1, (0,0,255), 5)
+        cv2.putText(img,str(idx), tuple(pt.astype(np.int)),FONT_HERSHEY_COMPLEX,1,(0,0,255))
+    # compute IPM matrix and apply it
+
+    point1x=img.shape[1]/2
+    point1y=img.shape[0]/2
+    point2x=img.shape[1]/2
+    point2y=img.shape[0]/2+50
+    point3x=img.shape[1]/2+50
+    point3y=img.shape[0]/2
+    point4x=img.shape[1]/2+50
+    point4y=img.shape[0]/2+50
+    print('ddddd')
+    # ipm_pts = np.array([[448,609], [580,609], [580,741], [448,741]], dtype=np.float32)
+    ipm_pts = np.array([[point2x,point2y], [point1x,point1y],[point4x,point4y],[point3x,point3y]], dtype=np.float32)
+    ipm_matrix = cv2.getPerspectiveTransform(pts, ipm_pts)
+    ipm = cv2.warpPerspective(img, ipm_matrix, img.shape[:2][::-1])
+    ipm = cv2.rotate(ipm, cv2.ROTATE_180) 
+
+    print('ipm')
+    print(ipm_matrix)
+    
+    # ipm90 = cv2.rotate(ipm, cv2.ROTATE_90_CLOCKWISE) 
+    # ipm180 = cv2.rotate(ipm, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    # display (or save) images
+    # cv2.imshow('img', img)
+    # cv2.imshow('img', img)
+   
+    cv2.waitKey()
+
+    return ipm
+
+
+# =====================================================================
+
+# DIM=(640, 480)
+# K=np.array([[236.09690171418842, 0.0, 318.85338449602176], [0.0, 234.89387612174056, 249.52451910875553], [0.0, 0.0, 1.0]])
+# D=np.array([[-0.044587316407145215], [-0.006867926932128537], [0.0010003423736737584], [-6.98647544665307e-05]])
+
+# fname = 'pjh/data/extrinsic_cart/sssssss1.png'
+
+# img = cv2.imread(fname)
+# # print(str(img))
+# undistorted_img = undistort(img, K, D, DIM) 
+# topview_img = topview(undistorted_img)
+# cv2.imshow(str(fname), undistorted_img)
+# cv2.imshow(str(fname)+'sss', topview_img) 
+# # cv2.imshow('ddddddddddddddd', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# # cv2.waitKey(0)
+# cv2.destroyAllWindows()
